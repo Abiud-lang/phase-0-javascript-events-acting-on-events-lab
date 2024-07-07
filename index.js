88b36266e4193c7a51c2daf90deb7eca6b035a2a
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-07-07 18:07:31 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    dodger.style.left = "50px";
+    
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  } 
+}
